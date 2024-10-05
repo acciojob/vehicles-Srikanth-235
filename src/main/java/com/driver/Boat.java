c134a872ea4c021d7f619db8ea5e6767d81daf84
Srikanth-235 <161904315+Srikanth-235@users.noreply.github.com>
2024-10-05 06:39:24 +0530

    Update Boat.java

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,11 +2,11 @@ package com.driver;
 
 class Boat implements WaterVehicle {
         String name="toofan";
-        int capacity=1000;
+        int cap=1000;
     public String getVehicleName(){
             return name;
     }
     public int  getVehicleCapacity(){
-        return capacity;
+        return cap;
     }
 }
